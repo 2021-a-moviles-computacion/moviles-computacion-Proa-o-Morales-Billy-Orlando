e112735b8bc1bf31052387ed814d79bcce792556
Billy <flaco_@hotmail.com.ar>
2021-06-30 01:32:47 -0500

    hasta crear empleado y guardar en el archivo

---
+++ DEBER_01_Proaño_Billy/src/main/kotlin/Menu/control-menu.kt
@@ -0,0 +1,5 @@
+package Menu
+
+class `control-menu` {
+
+}
--- DEBER_01_Proaño_Billy/src/main/kotlin/main.kt
+++ DEBER_01_Proaño_Billy/src/main/kotlin/main.kt
@@ -1,60 +1,19 @@
-import java.util.*
+import Control.crearEmpleadoMenu
 
 fun main(args: Array<String>) {
 
-    val arregloEmpresas = arrayListOf<Empresa>()
-
-    arregloEmpresas.add(
-        Empresa("1720540879001","PC REPAIRS DP",2476762, "13-07-2019",3000.00)
-    )
-    arregloEmpresas.add(
-        Empresa("1725969503001","MEDICENTER ",3441319, "25-10-2020",50000.00)
-    )
-    println(arregloEmpresas [1])
 
 
-    val arregloEmpleados = arrayListOf<Empleado>()
+    //val empleado = crearEmpleado("1725996365", "Billy Proaño", 2497782, "01-10-1997", 500.90, true)
 
-    arregloEmpleados.add(
-        Empleado("1725996365","Billy Proaño",983333483,"01-10-1997", 450.50)
-    )
-    arregloEmpleados.add(
-        Empleado("1708394000","Cecilia Morales",983333483,"16-12-1965", 652.00)
-    )
-    println(arregloEmpleados [0])
+    //escribirEnArchivo(empleado.toString())
+
+
+    //buscarEmpleados("dni","1725996365", listaEmpleado)
+
+    crearEmpleadoMenu()
 }
 
-//PRIMERA ENTIDAD
-class Empresa(
-    val ruc: String,
-    val razonSocial: String,
-    val telefono: Int,
-    val fechaInicio: String,
-    val capital: Double,
-){
-    override fun toString(): String {
-        return "Nombre: ${razonSocial} \n" +
-                "RUC: ${ruc} \n" +
-                "Telefono: ${telefono} \n" +
-                "Fecha de apertura: ${fechaInicio} \n" +
-                "Capital ${capital} \n"
-    }
-}
 
-//SEGUNDA ENTIDAD
-class Empleado(
-    val dni: String,
-    val nombre: String,
-    val telefono: Int,
-    val fechaNacimiento: String,
-    val sueldo: Double,
-){
-    override fun toString(): String {
-        return "Nombre: ${nombre} \n" +
-                "Cedula de Ciudadania: ${dni} \n" +
-                "Telefono: ${telefono} \n" +
-                "Fecha de nacimiento: ${fechaNacimiento} \n" +
-                "Sueldo ${sueldo} \n"
-    }
-}
+
 
