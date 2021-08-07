6e17ebe4c82750d5a4dc72840893042bd7c0d190
Billy <flaco_@hotmail.com.ar>
2021-08-06 22:38:41 -0500

    correciones distancia entre elemntos del list view

--- DEBER_02/app/src/main/java/com/example/deber_02_recyclerview/MainActivity.kt
+++ DEBER_02/app/src/main/java/com/example/deber_02_recyclerview/MainActivity.kt
@@ -10,7 +10,20 @@ class MainActivity : AppCompatActivity() {
     val contactos = listOf<Contacto>(
         Contacto("https://images.unsplash.com/photo-1547425260-76bcadfb4f2c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
             "Pablo",
+            "hola como estas, no he sabido de ti hace mucho tiempo"),
+        Contacto("https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+            "Ximena",
+            "hola como estas, no he sabido de ti hace mucho tiempo"),
+        Contacto("https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+            "Carla",
+            "hola como estas, no he sabido de ti hace mucho tiempo"),
+        Contacto("https://images.unsplash.com/photo-1552058544-f2b08422138a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=344&q=80",
+        "Mario",
+        "hola como estas, no he sabido de ti hace mucho tiempo"),
+        Contacto("https://images.unsplash.com/photo-1504593811423-6dd665756598?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+            "Marco",
             "hola como estas, no he sabido de ti hace mucho tiempo")
+
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
