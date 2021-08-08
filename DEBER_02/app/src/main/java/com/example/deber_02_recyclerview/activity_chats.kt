3093016dea4ccec9cc542a7423db44469c3a2b5b
Billy <flaco_@hotmail.com.ar>
2021-08-08 00:15:14 -0500

    estilos pantalla chat semi-completos

--- DEBER_02/app/src/main/java/com/example/deber_02_recyclerview/activity_chats.kt
+++ DEBER_02/app/src/main/java/com/example/deber_02_recyclerview/activity_chats.kt
@@ -2,7 +2,9 @@ package com.example.deber_02_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import org.w3c.dom.Text
 
 class activity_chats : AppCompatActivity() {
@@ -15,9 +17,11 @@ class activity_chats : AppCompatActivity() {
 
         val txtMensaje = findViewById<TextView>(R.id.txt_chat_mensaje)
         val txtNombre = findViewById<TextView>(R.id.txt_chat_nombre)
+        val img_chat = findViewById<ImageView>(R.id.img_chat)
 
         txtMensaje.text = contacto!!.mensaje
         txtNombre.text = contacto!!.nombre
+        Picasso.get().load(contacto!!.imagen).into(img_chat)
 
     }
 }
